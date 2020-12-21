20cd8117c76932139b578b5c982fa34e9269ce97
polemonlx <diogo.ventura6@hotmail.com>
2020-12-21 22:45:17 +0000

    Changed users to user

--- src/main/java/org/academiadecodigo/tailormoons/blue_ocean/persistence/model/security/User.java
+++ src/main/java/org/academiadecodigo/tailormoons/blue_ocean/persistence/model/security/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "users", schema = "public")
+@Table(name = "user", schema = "public")
 public class User extends AbstractModel {
 
     private String name;
