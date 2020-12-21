2d08ff939752fa2cd5dfbd7c63f200fb13a76df3
polemonlx <diogo.ventura6@hotmail.com>
2020-12-21 22:54:22 +0000

    Added a schema that fucking works

--- src/main/java/org/academiadecodigo/tailormoons/blue_ocean/persistence/model/security/User.java
+++ src/main/java/org/academiadecodigo/tailormoons/blue_ocean/persistence/model/security/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user", schema = "public")
 public class User extends AbstractModel {
 
     private String name;
