3000481a9ee78c913101e871bfd10e8581892923
Marco Dias <marcoagdias@gmail.com>
2020-12-30 20:47:00 +0000

    added thymeleaf to user-profile controller

--- src/main/java/org/academiadecodigo/tailormoons/blue_ocean/controller/web/MainController.java
+++ src/main/java/org/academiadecodigo/tailormoons/blue_ocean/controller/web/MainController.java
@@ -1,9 +1,14 @@
 package org.academiadecodigo.tailormoons.blue_ocean.controller.web;
 
+import org.academiadecodigo.tailormoons.blue_ocean.converter.CustomerToCustomerDto;
+import org.academiadecodigo.tailormoons.blue_ocean.converter.flag.FlagReviewedToFlagReviewedDto;
+import org.academiadecodigo.tailormoons.blue_ocean.converter.flag.FlagSubmittedToFlagSubmittedDto;
+import org.academiadecodigo.tailormoons.blue_ocean.persistence.model.Customer;
 import org.academiadecodigo.tailormoons.blue_ocean.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,12 +17,29 @@ public class MainController {
 
     private CustomerService customerService;
 
+    private CustomerToCustomerDto customerToCustomerDto;
+    private FlagSubmittedToFlagSubmittedDto flagSubmittedToFlagSubmittedDto;
+    private FlagReviewedToFlagReviewedDto flagReviewedToFlagReviewedDto;
 
     @Autowired
     public void setCustomerService(CustomerService customerService) {
         this.customerService = customerService;
     }
 
+    @Autowired
+    public void setCustomerToCustomerDto(CustomerToCustomerDto customerToCustomerDto) {
+        this.customerToCustomerDto = customerToCustomerDto;
+    }
+
+    @Autowired
+    public void setFlagSubmittedToFlagSubmittedDto(FlagSubmittedToFlagSubmittedDto flagSubmittedToFlagSubmittedDto) {
+        this.flagSubmittedToFlagSubmittedDto = flagSubmittedToFlagSubmittedDto;
+    }
+
+    @Autowired
+    public void setFlagReviewedToFlagReviewedDto(FlagReviewedToFlagReviewedDto flagReviewedToFlagReviewedDto) {
+        this.flagReviewedToFlagReviewedDto = flagReviewedToFlagReviewedDto;
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = "/lobby")
     public String getLobby(Model model) {
@@ -25,9 +47,15 @@ public class MainController {
         return "lobby";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/user-profile")
-    public String getUserProfile() {
-        // Thymeleaf HERE
+    @RequestMapping(method = RequestMethod.GET, path = "/user-profile/{id}")
+    public String getUserProfile(@PathVariable Integer id, Model model) {
+
+        Customer customer = customerService.get(id);
+
+        model.addAttribute("customer", customerToCustomerDto.convert(customer));
+        model.addAttribute("flagsSubmitted", flagSubmittedToFlagSubmittedDto.convert(customer.getFlagsSubmitted()));
+        model.addAttribute("flagsReviewed", flagReviewedToFlagReviewedDto.convert(customer.getFlagsReviewed()));
+
         return "user-profile";
     }
 
---
+++ src/main/java/org/academiadecodigo/tailormoons/blue_ocean/converter/flag/FlagSubmittedToFlagSubmittedDto.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.tailormoons.blue_ocean.converter.flag;
+
+import org.academiadecodigo.tailormoons.blue_ocean.converter.AbstractConverter;
+import org.academiadecodigo.tailormoons.blue_ocean.dto.flag.FlagSubmittedDto;
+import org.academiadecodigo.tailormoons.blue_ocean.persistence.model.flag.Flag;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlagSubmittedToFlagSubmittedDto extends AbstractConverter<Flag, FlagSubmittedDto> {
+
+    @Override
+    public FlagSubmittedDto convert(Flag flag) {
+
+        FlagSubmittedDto flagSubmittedDto = new FlagSubmittedDto();
+
+        flagSubmittedDto.setId(flag.getId());
+        flagSubmittedDto.setDescription(flag.getDescription());
+        flagSubmittedDto.setLat(flag.getLat());
+        flagSubmittedDto.setLng(flag.getLng());
+        flagSubmittedDto.setCustomerId(flag.getCustomer().getId());
+
+        return flagSubmittedDto;
+    }
+}
