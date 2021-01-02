c9dc6794527ca18014aa3f94d447ba380b2d901a
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-01-02 21:03:01 +0000

    update root db pass

--- src/main/java/org/academiadecodigo/tailormoons/blue_ocean/controller/web/MainController.java
+++ src/main/java/org/academiadecodigo/tailormoons/blue_ocean/controller/web/MainController.java
@@ -25,11 +25,6 @@ public class MainController {
         return "lobby";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/admin-panel")
-    public String getAdminPanel(Model model) {
-        model.addAttribute("customerId", 1);
-        return "admin-panel";
-    }
     @RequestMapping(method = RequestMethod.GET, path = "/user-profile")
     public String getProfile(Model model) {
         model.addAttribute("customerId", 1);
